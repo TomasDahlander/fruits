9f1be3245a49f540616c03e7a7fc5c81be227a7f
Tomas Dahlander <thomaz.dahlander@hotmail.com>
2021-04-15 15:20:25 +0200

    Added system.properties file to locla version

--- src/main/java/com/example/fruits/FruitsApplication.java
+++ src/main/java/com/example/fruits/FruitsApplication.java
@@ -1,7 +1,9 @@
 package com.example.fruits;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FruitsApplication {
@@ -9,5 +11,8 @@ public class FruitsApplication {
     public static void main(String[] args) {
         SpringApplication.run(FruitsApplication.class, args);
     }
+
+//    @Bean
+//    public CommandLineRunner demo()
 
 }
